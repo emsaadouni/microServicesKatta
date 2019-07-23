6438e8bd3161d83aca84e44bbb2bc51c4f71acea
emsaadouni <el-mahdi.saadouni@gmail.com>
2019-07-23 18:03:27 +0200

    Rest Controller Setup Done

--- mysvc/src/main/java/msvcdojo/mysvc/MysvcApplication.java
+++ mysvc/src/main/java/msvcdojo/mysvc/MysvcApplication.java
@@ -2,14 +2,24 @@ package msvcdojo.mysvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MysvcApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MysvcApplication.class, args);
 	}
 
+	@RestController
+	public static class HomeController {
+
+		@RequestMapping("/")
+		String home() {
+			return "Hello World!";
+		}
+	}
 
 }
-
